0b701fd7bdb1eafb2daa6ae182dd9fafa564722e
John Kastner <john.h.kastner@gmail.com>
2017-08-07 10:10:25 -0400

    Added green assessment edit page

--- helix/urls.py
+++ helix/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import patterns, url
-from helix.views import helix_home, helix_hes, helix_csv_upload, assessment_view
+from helix.views import (
+    helix_home,
+    helix_hes,
+    helix_csv_upload,
+    assessment_view,
+    assessment_edit)
 
 urlpatterns = patterns('',
     url(r'^$', helix_home, name='helix_home'),
     url(r'^helix-hes/$', helix_hes, name='helix_hes'),
     url(r'^helix-csv-upload/$', helix_csv_upload, name="helix_csv_upload"),
-    url(r'^assessments/$', assessment_view, name="assessment_view")
+    url(r'^assessments/$', assessment_view, name="assessment_view"),
+    url(r'^assessments/edit/$', assessment_edit, name="assessment_edit")
 )
