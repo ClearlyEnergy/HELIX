9517f2ec4160d250cbe62ceac9947c3846513649
John Kastner <john.h.kastner@gmail.com>
2017-08-11 16:00:24 -0400

    Adapted reso-xml-export to check for disclosure

--- helix/models.py
+++ helix/models.py
@@ -2,4 +2,6 @@ from django.db import models
 from seed.models import certification
 
 class HELIXGreenAssessmentProperty(certification.GreenAssessmentProperty):
-    disclosure = models.CharField(max_length=100) # null=true, blank=true might need to be set
+    # setting default disclosure to true. Worth considering weather this is
+    # should be the case.
+    disclosure = models.BooleanField(default=True) # null=true, blank=true might need to be set
