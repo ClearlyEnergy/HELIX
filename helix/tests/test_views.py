0dcbb64053121d67b0238595d224de863c52950d
John Kastner <john.h.kastner@gmail.com>
2017-08-14 14:41:16 -0400

    Tests for export with private data flag

--- helix/tests/test_views.py
+++ helix/tests/test_views.py
@@ -1,12 +1,10 @@
-import datetime
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.core import management
 from django.utils import timezone
 from seed.landing.models import SEEDUser as User
 from seed.lib.superperms.orgs.models import Organization, OrganizationUser
-from seed.models import Cycle
-from seed.models.certification import GreenAssessment
+from seed.models import Cycle, PropertyView
 from seed.data_importer.models import ImportRecord
 
 
@@ -60,16 +58,16 @@ class TestHelixView(TestCase):
     def test_helix_hes(self):
         data = {'user_key': self.user_key,
                 'building_id': self.building_id,
-                'dataset':self.record.pk,
-                'cycle':self.cycle.pk}
+                'dataset': self.record.pk,
+                'cycle': self.cycle.pk}
         res = self.client.get(reverse('helix:helix_hes'), data)
         self.assertEqual(200, res.status_code)
 
     def test_helix_hes_bad_id_400(self):
         data = {'user_key': self.user_key,
                 'building_id': 1425434,
-                'dataset':self.record.pk,
-                'cycle':self.cycle.pk}
+                'dataset': self.record.pk,
+                'cycle': self.cycle.pk}
 
         res = self.client.get(reverse('helix:helix_hes'), data)
         self.assertEqual(400, res.status_code)
@@ -77,8 +75,8 @@ class TestHelixView(TestCase):
     def test_helix_hes_bad_hes_key_400(self):
         data = {'user_key': 'ce4cdc28710349a1bbb4b7a047b65827',
                 'building_id': 142543,
-                'dataset':self.record.pk,
-                'cycle':self.cycle.pk}
+                'dataset': self.record.pk,
+                'cycle': self.cycle.pk}
 
         res = self.client.get(reverse('helix:helix_hes'), data)
         self.assertEqual(400, res.status_code)
@@ -86,13 +84,51 @@ class TestHelixView(TestCase):
     # Check that the most basic upload case returns the expected status
     # Does not verify the end state of the database is correct
     def test_helix_csv_upload(self):
-         with open('../helix_upload_sample.csv') as csv:
-             data = {'user_key': self.user_key,
+        with open('./helix/helix_upload_sample.csv') as csv:
+            data = {'user_key': self.user_key,
                     'building_id': 1425434,
-                    'dataset':self.record.pk,
-                    'cycle':self.cycle.pk,
-                    'helix_csv':csv}
-             res = self.client.post(reverse('helix:helix_csv_upload'), data)
-             self.assertEqual(302, res.status_code)
+                    'dataset': self.record.pk,
+                    'cycle': self.cycle.pk,
+                    'helix_csv': csv}
+        res = self.client.post(reverse('helix:helix_csv_upload'), data)
+        self.assertEqual(302, res.status_code)
 
+    def test_reso_export_with_private(self):
+        with open('./helix/helix_upload_sample.csv') as csv:
+            data = {'user_key': self.user_key,
+                    'building_id': 1425434,
+                    'dataset': self.record.pk,
+                    'cycle': self.cycle.pk,
+                    'helix_csv': csv}
+            res = self.client.post(reverse('helix:helix_csv_upload'), data)
+            self.assertEqual(302, res.status_code)
 
+            view_id = PropertyView.objects.get(state__custom_id_1='1234').pk
+
+            data = {'propertyview_pk': view_id,
+                    'start_date': '2016-09-14',
+                    'end_date': '2016-09-26',
+                    'private_data': 'True'}
+            res = self.client.get(reverse('helix:helix_reso_export_xml'), data)
+            self.assertEqual(200, res.status_code)
+            self.assertTrue('NGBS' in res.content)
+
+    def test_reso_export_no_private(self):
+        with open('./helix/helix_upload_sample.csv') as csv:
+            data = {'user_key': self.user_key,
+                    'building_id': 1425434,
+                    'dataset': self.record.pk,
+                    'cycle': self.cycle.pk,
+                    'helix_csv': csv}
+            res = self.client.post(reverse('helix:helix_csv_upload'), data)
+            self.assertEqual(302, res.status_code)
+
+            view_id = PropertyView.objects.get(state__custom_id_1='1234').pk
+
+            data = {'propertyview_pk': view_id,
+                    'start_date': '2016-09-14',
+                    'end_date': '2016-09-26',
+                    'private_data': 'True'}
+            res = self.client.get(reverse('helix:helix_reso_export_xml'), data)
+            self.assertEqual(200, res.status_code)
+            self.assertTrue('NGBS' in res.content)
