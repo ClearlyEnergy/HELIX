9cf19d9081c34142564c4dfd15c4272926f70aaf
ClearlyEnergy <vbugnion@clearlyenergy.com>
2017-10-11 17:05:07 -0400

    update to url pattern to avoid warning

--- helix/urls.py
+++ helix/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, url
 from helix.views import (
     helix_hes,
+    helix_hes_upload,
     helix_csv_upload,
     helix_csv_export,
     assessment_view,
@@ -8,12 +9,12 @@ from helix.views import (
     helix_reso_export_xml
 )
 
-
-urlpatterns = patterns('',
-    url(r'^helix-hes/$', helix_hes, name='helix_hes'),
-    url(r'^helix-csv-upload/$', helix_csv_upload, name="helix_csv_upload"),
+urlpatterns = [
     url(r'^assessments/$', assessment_view, name="assessment_view"),
     url(r'^assessments/edit/$', assessment_edit, name="assessment_edit"),
+    url(r'^helix-hes-upload/$', helix_hes_upload, name='helix_hes_upload'),
+    url(r'^helix-hes/$', helix_hes, name='helix_hes'),
+    url(r'^helix-csv-upload/$', helix_csv_upload, name="helix_csv_upload"),
     url(r'^helix-csv-export/$', helix_csv_export, name="helix_csv_export"),
-    url(r'^helix-reso-export-xml/$', helix_reso_export_xml, name="helix_reso_export_xml")
-)
+    url(r'^helix-reso-export-xml/$', helix_reso_export_xml, name="helix_reso_export_xml"),
+]
