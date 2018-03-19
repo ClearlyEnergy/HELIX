83e534c0df6f4fb940029f5b0689ba51368c4f49
ClearlyEnergy <vbugnion@clearlyenergy.com>
2018-03-19 15:12:37 -0400

    made certification upload more sturdy

--- helix/helix_utils.py
+++ helix/helix_utils.py
@@ -24,23 +24,21 @@ from helix.utils.address import normalize_address_str
 
 from autoload import autoload
 from hes import hes
-
-# Utility to create a dict that will be part of a column mapping list
-def mapping_entry(to_field, from_field):
-    return {'to_field': to_field,
-            'to_table_name': 'PropertyState',
-            'from_field': from_field}
     
 # Create certifications
 def helix_certification_create(user, file_pk):
-    # load some of the data directly from csv
+    """
+    Creates and saves certifications to the database
+    user        user object
+    file_pk     primary key of data file
+    Returns
+    results     dictionary with number of new and updated assessments and measurements
+    """
     loader = autoload.AutoLoad(user, user.default_organization)
     import_file = ImportFile.objects.get(pk=file_pk)
-
-    ### 1. read mappings to find mapped name - largely from seed/data_importer/tasks.py
     org = Organization.objects.get(pk=import_file.import_record.super_organization.pk)
     
-    ### 2. read mapped records from file_pk and map
+    #read mapped records from file_pk and map
     qs = list(
         PropertyState.objects.filter(import_file=import_file).exclude(
             data_state__in=[DATA_STATE_UNKNOWN, DATA_STATE_IMPORT]).only(
@@ -69,40 +67,49 @@ def helix_certification_create(user, file_pk):
 
             # long format
             if 'Green Assessment Name' in extra_data:
-                assessment = GreenAssessment.objects.get(name=extra_data['Green Assessment Name'], 
-                    organization=org)
-                green_assessment_data = {
-                    "source": extra_data['Green Assessment Property Source'],
-                    "version": extra_data['Green Assessment Property Version'],
-                    "date": cleaners.date_cleaner(extra_data['Green Assessment Property Date']),
-                    "status": extra_data['Green Assessment Property Status'],
-                    "extra_data": extra_data['Green Assessment Property Extra Data'],
-                    "urls": [extra_data['Green Assessment Property Url']],
-                    "assessment": assessment
+                gap_fields = {'Green Assessment Property Source': 'source', 
+                    'Green Assessment Property Version': 'version', 
+                    'Green Assessment Property Status': 'status', 
+                    'Green Assessment Reference Id': 'reference_id', 
+                    'Green Assessment Property Extra Data': 'extra_data', 
+                    'Green Assessment Property Url': 'urls'                   
                 }
-            
-                if 'Opt Out' in extra_data:
-                    green_assessment_data["opt_out"] = cleaners.bool_cleaner(extra_data['Opt Out'])
-                 
-                score_type = ("Metric" if assessment.is_numeric_score else "Rating") 
-                score_value = test_score_value(score_type, extra_data['Green Assessment Property '+score_type])
-                if score_value not in ['','FALSE']:
-                    green_assessment_data.update({score_type.lower(): score_value}) 
-           
-                    log, prop_assess = loader.create_green_assessment_property(
-                        green_assessment_data, normalized_address, postal_code)
-                    results['new_assessments'] += log['created']
-                    results['updated_assessments'] += log['updated']
+                try: 
+                    assessment = GreenAssessment.objects.get(name=extra_data['Green Assessment Name'], 
+                        organization=org)
+                    green_assessment_data = {"assessment": assessment}
+                    for key, value in gap_fields.items():
+                        if key in extra_data:
+                            green_assessment_data[value] = extra_data[key]
+                    
+                    if 'Green Assessment Property Date' in extra_data:
+                        green_assessment_data["date"] = cleaners.date_cleaner(extra_data['Green Assessment Property Date'])
 
-                log = _setup_measurements(extra_data, prop_assess)
-                results['new_measurements'] += log['created']
-                results['updated_measurements'] += log['updated']
+                    if 'Opt Out' in extra_data:
+                        green_assessment_data["opt_out"] = cleaners.bool_cleaner(extra_data['Opt Out'])
+                 
+                    score_type = ("Metric" if assessment.is_numeric_score else "Rating") 
+                    score_value = test_score_value(score_type, extra_data['Green Assessment Property '+score_type])
+                    if score_value not in ['','FALSE']:
+                        green_assessment_data.update({score_type.lower(): score_value}) 
+           
+                        log, prop_assess = loader.create_green_assessment_property(
+                            green_assessment_data, normalized_address, postal_code)
+                        results['new_assessments'] += log['created']
+                        results['updated_assessments'] += log['updated']
+
+                    log = _setup_measurements(extra_data, prop_assess)
+                    results['new_measurements'] += log['created']
+                    results['updated_measurements'] += log['updated']
+                except Exception as e:
+                    print e
+                    return {'status': 'error', 'message': str(e)}                    
+                    
             # short format
             else:
                 for assess, value in extra_data.items():
                     try: 
                         assessment = GreenAssessment.objects.get(name=assess, organization=org)
-                        print(assessment)
                         score_type = ("Metric" if assessment.is_numeric_score else "Rating")
                         score_value = test_score_value(score_type, value)
                         if score_value not in ['','FALSE']:
@@ -120,8 +127,18 @@ def helix_certification_create(user, file_pk):
             
     return {'status': 'success', 'data': results}
 
-# retrieves home energy score records and formats file for rest of upload process
 def helix_hes_to_file(user, dataset, cycle, hes_auth, partner, start_date=None):
+    """
+    Retrieves home energy score records and formats file for rest of upload process
+    user        user object
+    dataset     reference dataset to attach records to
+    cycle       reference cycle to attach records to
+    hes_auth    Home Energy Score authentication
+    partner     Name of Home Energy Score to retrieve data for
+    start_date  Date to start pulling Home Energy Score records from
+    Returns
+    results     dictionary with status and primary key of file created
+    """
     # instantiate HES client for external API
     hes_client = hes.HesHelix(hes_auth['client_url'], hes_auth['user_name'], hes_auth['password'], hes_auth['user_key'])
     # find assessment entry for hes by name. Maybe not ideal!
@@ -199,6 +216,10 @@ def test_score_value(score_type, value):
             return value.strip().upper()
                 
 def _setup_measurements(extra_data, assessment_property):
+    """Creates dictionary with measuremet data
+    :param extra_data           source dict for data
+    :param assessment_property  source property
+    """
     data = {
         'created': 0,
         'updated': 0,
@@ -209,6 +230,7 @@ def _setup_measurements(extra_data, assessment_property):
             if not dat:
                 continue
                  
+            print dat
             dat = json.loads(dat)
             # find fuel and measurement type
             for fuel in list(HelixMeasurement.HES_FUEL_TYPES.keys()):
@@ -219,20 +241,32 @@ def _setup_measurements(extra_data, assessment_property):
                 'measurement_type': k.split(' ')[0],
                 'fuel':  HelixMeasurement.HES_FUEL_TYPES[fuel],
                 'quantity': dat['quantity'],
-                'unit': HelixMeasurement.HES_UNITS[dat['unit']], 
-                'status': dat['status']}
-            if 'pv' in k:
-                measurement_data['measurement_subtype'] = 'PV'
-                if k.startswith('CAP'):
-                    measurement_data['year'] = dat['year']
+                'unit': HelixMeasurement.HES_UNITS[dat['unit']]
+            }
+                
+            if 'status' in dat:
+                measurement_data['status'] = dat['status']
+                
+            if 'subtype' in dat:
+                measurement_data['measurement_subtype'] = dat['subtype']
+                
+            if 'year' in dat:
+                measurement_data['year'] = dat['year']
+            
+            print measurement_data
                     
             data_log, meas = _create_measurement(assessment_property, **measurement_data)
+            print 'after create measurement'
             data['created'] += data_log['created']
             data['updated'] += data_log['updated']
 
     return data
         
 def _create_measurement(assessment_property, **kwargs):
+    """Creates measurement record
+    :param assessment_property  source property
+    :param kwargs   measurement data dictionary
+    """
     data_log = {'created': False, 'updated': False}
     kwargs.update({'assessment_property': assessment_property})
     measurement_record, created = HelixMeasurement.objects.get_or_create(**kwargs)
